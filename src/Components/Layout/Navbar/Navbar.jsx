9a9654724316b4d634b4fe52f2fa0b03b4312ca6
yasinarafatwebdeveloper <yasinsahazi99@gmail.com>
2024-12-31 11:03:29 +0600

    work is very updating

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
 
     const navItems=<>
     
-    <li className="font-bold text-base sm:mr-3"><Link to="/">Home</Link></li>
-    <li className="font-bold text-base sm:mr-3"><Link to="/about">About</Link></li>
-    <li className="font-bold text-base sm:mr-3"><Link to="/rooms">Rooms</Link></li>
-    <li className="font-bold text-base sm:mr-3"><Link to="/">Blogs</Link></li>
-    <li className="font-bold text-base sm:mr-3"><Link to="/">Page</Link></li>
+    <li className="font-bold text-base "><Link to="/">Home</Link></li>
+    <li className="font-bold text-base "><Link to="/about">About</Link></li>
+    <li className="font-bold text-base "><Link to="/rooms">Rooms</Link></li>
+    <li className="font-bold text-base "><Link to="/">Blogs</Link></li>
+    <li className="font-bold text-base "><Link to="/">Page</Link></li>
     </>
     return (
         <div className="h-[100px] 
@@ -38,7 +38,7 @@ drop-shadow-md sticky top-0 left-0 z-50">
       </ul>
     </div>
     {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
-    <img className=" xl:w-[181px] xl:h-24 lg:w-[181px] lg:h-24 md:w-[181px] md:h-24 sm:w-[100px] sm:h-14 " src={logo} alt="" />
+    <img className=" xl:w-[181px] xl:h-24 lg:w-[181px] lg:h-24 md:w-[181px] md:h-24 sm:w-[140px] sm:h-14 w-[100px] h-[50px] " src={logo} alt="" />
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -54,15 +54,16 @@ drop-shadow-md sticky top-0 left-0 z-50">
   <div className="navbar-end">
 
 
-    <ul className="flex lg:px-10 sm:px-5 ">
-    <li className=" font-semibold text-base sm:px-2 px-3"><Link to="/">Login</Link></li>/
-    <li className=" font-semibold text-base sm:px-2 px-3"><Link to="/">Signup</Link></li>
+    <ul className="flex ">
+    <li className=" lg:font-semibold md:font-semibold sm:font-semibold font-normal text-base sm:px-2 px-3"><Link className="px-0 mx-0" to="/">Login</Link></li>
+    <li className=" lg:font-semibold md:font-semibold sm:font-semibold font-normal text-base sm:px-2 px-3"><Link className="mx-0 my-0" to="/">Signup</Link></li>
     </ul>
  
 
+    {/*  */}
+    {/* btn   xl:w-32 xl:h-14 lg:w-32 lg:h-14 md:w-32 md:h-14 */}
 
-
-  <button className="btn btn-active bg-[#CEAF83] xl:w-32 xl:h-14 lg:w-32 lg:h-14 md:w-32 md:h-14 text-white font-bold rounded-full">Reservation</button>
+  <button className=" px-3 py-3 lg:px-3 lg:py-3 md:px-3 md:py-3 sm:px-4 sm:py-4 bg-[#CEAF83] font-bold rounded-full">Reservation</button>
   </div>
 </div>
         </div>
